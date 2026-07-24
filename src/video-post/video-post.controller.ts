20da1549f4a95dfa35bdc5afdc2c1463b766dba0
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2026-07-24 22:11:38 +0800

    add saving files locally

--- src/video-post/video-post.controller.ts
+++ src/video-post/video-post.controller.ts
@@ -14,13 +14,31 @@ import { CreateVideoPostDto } from './dto/create-video-post.dto';
 import { UpdateVideoPostDto } from './dto/update-video-post.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadVideoDto } from './dto/upload-video-post.dto';
+import { ApiConsumes } from '@nestjs/swagger';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('video-post')
 export class VideoPostController {
   constructor(private readonly videoPostService: VideoPostService) {}
 
   @Post('/upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multifile/form-data')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const suffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          cb(null, `${file.fieldname}-${suffix}${ext}`);
+        },
+      }),
+      limits: {
+        fileSize: 1000 * 1024 * 1024,
+      },
+    }),
+  )
   async upload(
     @UploadedFile() file: Express.Multer.File,
     @Body() uploadVideoDto: UploadVideoDto,
