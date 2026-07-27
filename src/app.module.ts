a7baf6f526a24a72319dc52a0ed87c02c3e33ea0
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2026-07-27 23:37:12 +0800

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -58,7 +58,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
       },
     ]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
